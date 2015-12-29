4baf884dfd9f4cd87047dbb7c37060e55dbb84c3
yuk7 <yukx00@gmail.com>
2015-12-29 18:51:32 +0900

    add command-line interface

--- main.cpp
+++ main.cpp
@@ -1,9 +1,48 @@
 #include <QCoreApplication>
+#include <QCommandLineParser>
+#include <iostream>
+#include <iomanip>
+#include "lvm.h"
+#include "ext2read.h"
+#include "ext2fs.h"
+
 
 int main(int argc, char *argv[])
 {
     QCoreApplication a(argc, argv);
+    QCoreApplication::setApplicationName("crext");
+    QCoreApplication::setApplicationVersion("dev-1.0");
 
-    return a.exec();
+    Ext2Read *app;
+    app = new Ext2Read();
+    log_init();
+
+    QCommandLineParser parser;
+    parser.setApplicationDescription("crext is command-line based ext image/partition reader.");
+    parser.addHelpOption();
+    parser.addVersionOption();
+
+    QCommandLineOption co_openfd("o","Open File/Devices","FilePath|dev");
+    parser.addOption(co_openfd);
+
+    QCommandLineOption co_listparts("ip","List Partition indexes");
+    parser.addOption(co_listparts);
+
+    QCommandLineOption co_setpart("sp","Set partition","Partition index","0");
+    parser.addOption(co_setpart);
+
+
+    QCommandLineOption co_cmdls("ls","List information files","-d|-l");
+    parser.addOption(co_cmdls);
+
+    QCommandLineOption co_cmdcopy("cp","copy file/directory");
+    parser.addOption(co_cmdcopy);
+
+    parser.process(a);
+
+
+
+
+    return 0;
 }
 
