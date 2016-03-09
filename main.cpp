d8dd647b5d80a9fe51808209746a41b2cb845775
yuk7 <yukx00@gmail.com>
2016-03-09 21:08:12 +0900

    main.cpp :add ctime command

--- main.cpp
+++ main.cpp
@@ -39,7 +39,7 @@ int main(int argc, char *argv[])
     QCommandLineOption co_setpart(QStringList() << "s" << "sp","Set Partition","Partition name","0");
     parser.addOption(co_setpart);
 
-    QCommandLineOption co_cmd(QStringList() << "c" << "cmd","Command","ls|lsl|cp|size|mode");
+    QCommandLineOption co_cmd(QStringList() << "c" << "cmd","Command","ls|lsl|cp|size|mode|ctime");
     parser.addOption(co_cmd);
 
     parser.addPositionalArgument("ePath", "Source Path(Ext Partition)");
@@ -289,7 +289,11 @@ int main(int argc, char *argv[])
         cout << mode_str(setefile->inode.i_mode) << endl;
         return 0;
     }
-
+    if(optcmd == "ctime")
+    {
+        cout << time_str(setefile->inode.i_ctime,"%Y-%m-%d %I:%M:%S") << endl;
+        return 0;
+    }
 
     cout << "bad parameter" << endl;
     cout << "The specified command option does not exist." << endl << endl;
